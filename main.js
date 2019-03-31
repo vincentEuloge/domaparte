cc3d67f0cc8eff46d8b334fdb6f20845d57203d1
Vincent Euloge <vincent.euloge@gmail.com>
2019-03-31 14:42:05 +0200

    fix js

--- main.js
+++ main.js
@@ -47,5 +47,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(port, function () {
-  console.log(`Example app listening on port ${port]!`)
+  console.log(`Example app listening on port ${port}`)
 })
