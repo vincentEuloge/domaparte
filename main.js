7e3a2535605ff7f25e1f23af152158f01376e5cc
Vincent Euloge <vincent.euloge@gmail.com>
2020-08-17 09:35:29 +0200

    (feat): use temperatures api

--- main.js
+++ main.js
@@ -1,39 +1,18 @@
-const http = require('http');
-/*const {BRIDGE_IP, HUE_KEY} = require("./domaparte.conf.json");
+require('dotenv').config()
+const fetch = require('node-fetch');
+
+const TEMPERATURES_API_URL = process.env.TEMPERATURES_API_URL || '';
+
+async function getTemp(){
+    const response = await fetch(TEMPERATURES_API_URL);
+    const json = await response.json();
+    return json
+        .values
+        .filter(([sensorName]) => sensorName === "capteur inutilisé")
+        .map(([, temperature]) => temperature)
+}
 
 
-http.get(`http://${BRIDGE_IP}/api/${HUE_KEY}/sensors`, (resp) => {
-  let data = '';
-
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-    const result = Object.values(JSON.parse(data));
-
-    const temperatureSensor = result
-      .filter(({type}) => type === "ZLLTemperature")
-      .map(({uniqueid, state: {temperature}}) => ({uniqueid, temperature}));
-
-    const namedtemperatureSensor = temperatureSensor
-      .map(({uniqueid, temperature}) => {
-        sensorName = result
-          .find(({uniqueid: presenceUniqueId, type}) => 
-            presenceUniqueId && type === "ZLLPresence" && presenceUniqueId.includes(uniqueid.slice(0,-1))
-          ).name;
-        
-          return {uniqueid, temperature, name: sensorName}
-      });
-
-    console.log(namedtemperatureSensor)
-  });
-
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});*/
 
 const express = require('express')
 const app = express()
@@ -42,8 +21,8 @@ if (port == null || port == "") {
   port = 8000;
 }
 
-app.get('/', function (req, res) {
-    res.send("Hello poupette!");
+app.get('/', async function (req, res) {
+    res.send(await getTemp());
 })
 
 app.listen(port, function () {
